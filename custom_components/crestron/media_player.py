17d67487125c622598ace29662dc223c51d44d6f
Thomas Wiepking <thomasw96@live.nl>
2023-05-21 22:15:05 +0200

    Changed media player to work with AACS

--- custom_components/crestron/media_player.py
+++ custom_components/crestron/media_player.py
@@ -1,16 +1,17 @@
 """Platform for Crestron Media Player integration."""
+"""Modified for use for Crestron AADS"""
 
 import voluptuous as vol
 import logging
+import asyncio
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.media_player import (
     MediaPlayerEntity,
-    SUPPORT_SELECT_SOURCE,
     SUPPORT_TURN_OFF,
     SUPPORT_TURN_ON,
     SUPPORT_VOLUME_MUTE,
-    SUPPORT_VOLUME_SET,
+    SUPPORT_VOLUME_STEP,
 )
 from homeassistant.const import STATE_ON, STATE_OFF, CONF_NAME
 from .const import (
@@ -18,8 +19,10 @@ from .const import (
     DOMAIN,
     CONF_MUTE_JOIN,
     CONF_VOLUME_JOIN,
-    CONF_SOURCE_NUM_JOIN,
-    CONF_SOURCES,
+    CONF_VOLUME_UP_JOIN,
+    CONF_VOLUME_DOWN_JOIN,
+    CONF_OFF_JOIN,
+    CONF_SOURCE_NUM_JOIN
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -35,8 +38,10 @@ PLATFORM_SCHEMA = vol.Schema(
         vol.Required(CONF_NAME): cv.string,
         vol.Required(CONF_MUTE_JOIN): cv.positive_int,           
         vol.Required(CONF_SOURCE_NUM_JOIN): cv.positive_int,           
-        vol.Required(CONF_VOLUME_JOIN): cv.positive_int,
-        vol.Required(CONF_SOURCES): SOURCES_SCHEMA,
+        vol.Required(CONF_VOLUME_UP_JOIN): cv.positive_int,
+        vol.Required(CONF_VOLUME_DOWN_JOIN): cv.positive_int,
+        vol.Required(CONF_OFF_JOIN): cv.positive_int,
+        vol.Required(CONF_VOLUME_JOIN): cv.positive_int
     },
     extra=vol.ALLOW_EXTRA,
 )
@@ -53,15 +58,17 @@ class CrestronRoom(MediaPlayerEntity):
         self._name = config.get(CONF_NAME)
         self._device_class = "speaker"
         self._supported_features = (
-            SUPPORT_SELECT_SOURCE
-            | SUPPORT_VOLUME_MUTE
-            | SUPPORT_VOLUME_SET
+            SUPPORT_VOLUME_MUTE
+            | SUPPORT_VOLUME_STEP
             | SUPPORT_TURN_OFF
+            | SUPPORT_TURN_ON
         )
         self._mute_join = config.get(CONF_MUTE_JOIN)
-        self._volume_join = config.get(CONF_VOLUME_JOIN)
+        self._volume_up_join = config.get(CONF_VOLUME_UP_JOIN)
+        self._volume_down_join = config.get(CONF_VOLUME_DOWN_JOIN)
+        self._volume_level_join = config.get(CONF_VOLUME_JOIN)
         self._source_number_join = config.get(CONF_SOURCE_NUM_JOIN)
-        self._sources = config.get(CONF_SOURCES)
+        self._off_join = config.get(CONF_OFF_JOIN)
 
     async def async_added_to_hass(self):
         self._hub.register_callback(self.process_callback)
@@ -81,6 +88,10 @@ class CrestronRoom(MediaPlayerEntity):
         return self._name
 
     @property
+    def unique_id(self):
+       return 'media-player-' + str(self._source_number_join)
+
+    @property
     def should_poll(self):
         return False
 
@@ -93,20 +104,8 @@ class CrestronRoom(MediaPlayerEntity):
         return self._supported_features
 
     @property
-    def source_list(self):
-        return list(self._sources.values())
-
-    @property
-    def source(self):
-        source_num = self._hub.get_analog(self._source_number_join)
-        if source_num == 0:
-            return None
-        else:
-            return self._sources[source_num]
-
-    @property
     def state(self):
-        if self._hub.get_analog(self._source_number_join) == 0:
+        if self._hub.get_digital(self._off_join) == 0:
             return STATE_OFF
         else:
             return STATE_ON
@@ -117,18 +116,29 @@ class CrestronRoom(MediaPlayerEntity):
 
     @property
     def volume_level(self):
-        return self._hub.get_analog(self._volume_join) / 65535
+        return self._hub.get_analog(self._volume_level_join) / 65535
 
     async def async_mute_volume(self, mute):
-        self._hub.set_digital(self._mute_join, mute)
+        self._hub.set_digital(self._mute_join, 1)
+        await asyncio.sleep(0.05)
+        self._hub.set_digital(self._mute_join, 0)
 
-    async def async_set_volume_level(self, volume):
-        self._hub.set_analog(self._volume_join, int(volume * 65535))
-
-    async def async_select_source(self, source):
-        for input_num, name in self._sources.items():
-            if name == source:
-                self._hub.set_analog(self._source_number_join, input_num)
+    async def async_turn_on(self):
+        self._hub.set_digital(self._source_number_join, 1)
+        await asyncio.sleep(0.05)
+        self._hub.set_digital(self._source_number_join, 0)
 
     async def async_turn_off(self):
-        self._hub.set_analog(self._source_number_join, 0)
+        self._hub.set_digital(self._off_join, 1)
+        await asyncio.sleep(0.05)
+        self._hub.set_digital(self._off_join, 0)
+
+    async def async_volume_up(self):
+        self._hub.set_digital(self._volume_up_join, 1)
+        await asyncio.sleep(0.05)
+        self._hub.set_digital(self._volume_up_join, 0)
+
+    async def async_volume_down(self):
+        self._hub.set_digital(self._volume_down_join, 1)
+        await asyncio.sleep(0.05)
+        self._hub.set_digital(self._volume_down_join, 0)
